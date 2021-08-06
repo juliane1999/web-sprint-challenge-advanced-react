ff7541630abdd410094cd4e5420c2103b34afaa8
juliane1999 <lastjuliane@gmail.com>
2021-08-06 13:51:00 -0700

    created the project and editing

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,18 +1,29 @@
 import React, { Component } from "react";
 import axios from "axios";
 
-export default class PlantList extends Component {
+export default class PlantList extends React.Component {
   // add state with a property called "plants" - initialize as an empty array
 
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
+constructor() {
+  super();
+  this.state = {
+    plants: []
+  }
+}
+
+ 
 
   componentDidMount() {
     axios.get('http://localhost:3333/plants')
       .then(res => {
-        
-      });
+        this.setState({
+          plants: res.data,
+        })
+      })
+      .catch(err => console.log(err));
   }
 
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,40 @@
 // write your custom hook here to control your checkout form
+import { useState } from "react";
+
+// const useLocalStorage = (key, initialValue) => {
+//     const [value,setValue] = useState(() => {
+//         if(localStorage.getItem(key)) {
+//             return(JSON.parse(localStorage.getItem(key)));
+//         }
+
+//         localStorage.setItem(key, JSON.stringify(initialValue));
+//         return(initialValue);
+//     });
+
+//     const setLocalStorageValue = (value) => {
+//         setValue(value);
+//         localStorage.setItem(key, JSON.stringify(value));
+//     }
+
+//     return [value,setLocalStorage];
+// }
+
+const useForm = (initialValue) => {
+    const [values,setValues] = useState( initialValue);
+
+    const handleChanges = e => {
+        setValues({
+            ...values,
+            [e.target.name]:e.target.value
+        });
+    };
+
+    // const clearForm = e => {
+    //     e.preventDefault();
+    //     setValues(initialValue);
+    // };
+
+    return [values,handleChanges];
+}
+
+export default useForm;
